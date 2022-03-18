53b959ff599a9d9d0ed308c3b7fdc41cfba9b7b6
williamsartijose <williamsartijose@hotmail.com>
2022-03-18 08:54:53 -0300

    [SEÇÃO 4 - ESTRUTURA SEQUENCIAL] - Processamento de dados Casting

---
+++ src/Exemplo2.java
@@ -0,0 +1,18 @@
+
+public class Exemplo2
+{
+    public static void main(
+        String[] agrs )
+    {
+
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println( x );
+        System.out.println( y );
+    }
+
+}
---
+++ src/Exemplo4.java
@@ -0,0 +1,19 @@
+public class Exemplo4
+{
+
+    public static void main(
+        String[] agrs )
+    {
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = a / b;
+
+        System.out.println( resultado );
+    }
+
+}
